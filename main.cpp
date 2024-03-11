911a3b7062d1ffe522c6f674dbb71aaada2199c2
globbertot <95583040+globbertot@users.noreply.github.com>
2024-03-11 18:59:56 +0200

    Update main.cpp V0.3

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    window w;
+    window w(nullptr);
     w.setWindowFlags(Qt::Window | Qt::FramelessWindowHint);
     w.show();
 
